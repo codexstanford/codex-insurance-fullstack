e5b4fe42df722848336c91b69bffd81b047693d8
alissav <63168432+alissav30@users.noreply.github.com>
2024-05-12 02:31:19 -0700

    revert back date picker
    lost some changes when i merged main into this branch so just restoring date picker fixes i had made

--- apps/react/src/components/InputDate.tsx
+++ apps/react/src/components/InputDate.tsx
@@ -1,11 +1,12 @@
-import { DateTime } from "luxon";
-import { useCallback, useMemo, useRef, useState } from "react";
-import { FaRegCalendarAlt } from "react-icons/fa";
+import { useCallback, useContext, useMemo, useState, useRef, useEffect } from "react";
 import Datepicker from "tailwind-datepicker-react";
-import { COMMON_INPUT_CLASSES } from "../consts/classes.const";
-import { classNames } from "../utils/classNames";
-import { stringToDate } from "../utils/epilogUtils";
 import { getButtonClassNames } from "./Button";
+import { classNames } from "../utils/classNames";
+import { COMMON_INPUT_CLASSES } from "../consts/classes.const";
+import { InputContext } from "../contexts/inputContext.ts";
+import { DateTime } from "luxon";
+import { FaRegCalendarAlt } from 'react-icons/fa';
+import { stringToDate } from "../utils/epilogUtils";
 
 // https://github.com/OMikkel/tailwind-datepicker-react
 
@@ -23,96 +24,43 @@ type InputDateProps = {
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
-// We have to override the stuff that comes from Datepicker
-const buttonClassNames = classNames(
-  getButtonClassNames("gray"),
-  "rounded-none",
-);
+const InputDate: React.FC = ({
+    value,
+    onChange,
+    onBlur,
+    ...props
+  }) => {
+    const inputContext = useContext(InputContext);
+    const [inputValue, setInputValue] = useState(value);
+  
+    const [show, setShow] = useState(false);
+    const datePickerWrapperRef = useRef(null);
+  
+    const handleChange = useCallback((e) => {
+        const newDateValue = e.target.value; // "YYYY-MM-DD" format
+        setInputValue(newDateValue);
+        if (newDateValue.match(/^\d{4}-\d{2}-\d{2}$/)) {
+          onChange(stringToDate(newDateValue.replace(/-/g, "_"))); // Convert to "DD_MM_YYYY" format if needed
+        }
+      }, [onChange]);
+  
 
-const InputDate: React.FC<InputDate_Input> = ({
-  options,
-  value,
-  onChange,
-  ...props
-}) => {
-  const [show, setShow] = useState(false);
-  const datePickerWrapperRef = useRef(null);
 
-  const onChangeCallback = useCallback(
-    (date) => {
-      const dateString = DateTime.fromJSDate(date).toISODate();
-      setInputValue(dateString);
-      onChange?.(date);
-      // Keep the calendar open even after selecting a date
-    },
-    [onChange],
-  );
-
-  const opts = useMemo(
-    () => ({
-      ...options,
-      theme: {
-        selected: "bg-blue-500 hover:bg-blue-700 text-white",
-      },
-    }),
-    [options],
-  );
-
-  const [inputValue, setInputValue] = useState(
-    DateTime.fromJSDate(value).toISODate(),
-  );
-
-  return (
-    <>
-      <div className="relative">
-        <button
-          onClick={() => setShow(!show)}
-          className="absolute right-0 top-0 mr-3 mt-2"
-        >
-          <FaRegCalendarAlt className="text-xl" /> {/* Calendar icon */}
-        </button>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-          }}
-          onBlur={(e) => {
-            // Check whether in correct format
-            const pattern: RegExp = /^\d{4}-\d{2}-\d{2}$/;
-            if (!pattern.test(inputValue)) {
-              return;
-            }
-            // Check whether it's a valid date
-            if (Number.isNaN(Date.parse(inputValue))) {
-              return;
-            }
-
-            const [year, month, day] = inputValue.split("-");
-
-            onChangeCallback(stringToDate(`${day}_${month}_${year}`));
-          }}
-          className={COMMON_INPUT_CLASSES + " w-full"}
-          placeholder="YYYY-MM-DD"
-        />
-        {show && (
-          <div className="date-picker-wrapper" ref={datePickerWrapperRef}>
-            <Datepicker
-              {...props}
-              show={show}
-              setShow={setShow}
-              onChange={onChangeCallback}
-              options={opts}
+      return (
+        <label style={{ display: 'block', marginBottom: '10px', fontSize: '16px' }}>
+          <div style={{ position: 'relative' }}>
+            <input
+              type="date"
+              name="coverageStartDate"
+              value={inputValue}
+              onChange={handleChange}
+              onBlur={onBlur}
+              className={COMMON_INPUT_CLASSES + " pl-4 pr-2 py-2 border rounded"}
+              style={{ display: 'block', width: '100%', fontSize: '16px', lineHeight: '20px' }}
             />
-            <button
-              onClick={() => setShow(false)}
-              className="absolute top-0 right-0 mt-2 mr-2 text-lg"
-            ></button>
           </div>
-        )}
-      </div>
-    </>
-  );
-};
-
-export default InputDate;
+        </label>
+      );
+    };
+    
+    export default InputDate;
