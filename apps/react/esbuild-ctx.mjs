75041acba59da31aedd4f59a3d4613fd7f676046
elterpro <elterpro@gmail.com>
2024-05-21 18:30:26 -0700

    fix

--- apps/react/esbuild-ctx.mjs
+++ apps/react/esbuild-ctx.mjs
@@ -2,7 +2,7 @@ import * as esbuild from "esbuild";
 import { copy } from "esbuild-plugin-copy";
 
 export const ctx = await esbuild.context({
-  entryPoints: ["./src/App.tsx"],
+  entryPoints: ["./src/app.tsx"],
   bundle: true,
   sourcemap: true,
   outfile: "./build/app.js",
